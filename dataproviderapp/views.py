1ae35b21bb0f579006a7d8c2862ac62fd1d50bf7
Ninad Parab <ndparab240@gmail.com>
2020-01-11 07:25:44 +0000

    Reverted

--- dataproviderapp/views.py
+++ dataproviderapp/views.py
@@ -105,10 +105,6 @@ class PropertyStatusViewSet(viewsets.ModelViewSet):
 	queryset = PropertyStatus.objects.all()
 	serializer_class = PropertyStatusSerializer
 
-def recommendproperty(request, str):
-  
-  return render(request, 'main/index.html')
-
 
 
 
